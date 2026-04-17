c602544b1b505a3a778f86434b2555ced3823c54
gap-anything <gapbreak@gmail.com>
2026-04-17 15:15:14 +0900

    __init__.py 추가

---
+++ collectors/새 텍스트 문서.py
@@ -0,0 +1,5 @@
+pythonfrom collectors.shinsegae import ShinsegaeCollector
+from collectors.hyundai   import HyundaiCollector
+from collectors.lotte     import LotteCollector
+
+ALL_COLLECTORS = [ShinsegaeCollector, HyundaiCollector, LotteCollector]
